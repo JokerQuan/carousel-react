bba680189726840cb6fc77f8a3f80e3f82bf7595
Jair <785744671@qq.com>
2022-12-19 21:23:18 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Carousel from './components/Carousel'
+
+function App() {
+
+  return (
+    <div className="app">
+      <Carousel />
+    </div>
+  )
+}
+
+export default App
