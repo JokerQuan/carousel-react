af4e0c645a84a844f3c1b3f999a9d9ba0295ba32
Jair <785744671@qq.com>
2022-12-20 15:52:04 +0800

    auto carousel

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,26 @@ import './App.css'
 import Carousel from './components/Carousel'
 
 function App() {
+  const imgData = [{
+    link: "https://www.baidu.com",
+    src: "https://img.alicdn.com/imgextra/i1/6000000002041/O1CN01Dw6SBo1QwpAedJV7p_!!6000000002041-0-octopus.jpg",
+  }, {
+    link: "https://www.zhihu.com",
+    src: "https://img.alicdn.com/imgextra/i2/6000000003292/O1CN0148GwRI1aBmeLAqrab_!!6000000003292-0-octopus.jpg",
+  }, {
+    link: "https://www.github.com",
+    src: "https://img.alicdn.com/imgextra/i2/6000000000160/O1CN01ZTeEW51D3KAkJ3ylC_!!6000000000160-0-octopus.jpg",
+  }, {
+    link: "https://www.google.com",
+    src: "https://img.alicdn.com/imgextra/i2/6000000003159/O1CN01dTNCKU1ZCrySxTRtj_!!6000000003159-2-octopus.png",
+  }, {
+    link: "https://www.weibo.com",
+    src: "https://img.alicdn.com/imgextra/i4/6000000003767/O1CN01KUtSXc1dhKmYeYeIK_!!6000000003767-2-octopus.png",
+  }]
 
   return (
     <div className="app">
-      <Carousel />
+      <Carousel data={imgData} width={550} height={300} />
     </div>
   )
 }
--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import './index.css'
 
-export default function Carousel() {
+export interface ImgData {
+  link?: string;
+  src: string;
+}
+
+export interface ICarouselProps {
+  data: Array<ImgData>;
+  width: number;
+  height: number;
+}
+
+export default function Carousel(props: ICarouselProps) {
+  const { data, width, height } = props
+  const [current, setCurrent] = useState(0);
+  const sliderRef = useRef<HTMLDivElement>(null)
+  const timer = useRef(0)
+  useEffect(() => {
+    timer.current = setTimeout(next, 2000)
+    return () => clearTimeout(timer.current)
+  }, [current])
+
+  const next = () => {
+    go(current + 1)
+  }
+
+  const go = (index: number) => {
+    sliderRef.current!.style.transition = `ease 1s`
+    sliderRef.current!.style.transform = `translateX(${- index * width}px)`
+    setCurrent(index)
+  }
+
   return (
-    <div>Carousel</div>
+    <div className='carousel' style={{width: `${width}px`, height: `${height}px`}}>
+      <div className='slider' ref={sliderRef} style={{width: `${data.length * width}px`}}>
+      {
+        data.map(img => (
+          <div key={img.src} className="slice">
+            <img src={img.src} alt="" width={width} height={height}/>
+          </div>
+        ))
+      }
+      </div>
+    </div>
   )
 }
