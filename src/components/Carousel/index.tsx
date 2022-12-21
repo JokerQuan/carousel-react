f82c5cde150c10ac38dbf7814c68127824bd2fd1
Jair <785744671@qq.com>
2022-12-21 20:30:15 +0800

    refactor timer code and support pause when hover

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -14,6 +14,7 @@ export interface ICarouselProps {
   pauseDuration?: number;
   slideDuration?: number;
   direction?: 1 | -1;
+  pauseOnHover?: boolean;
   tweenAnime?: "linear" | "ease" | "ease-in" | "ease-out" | "ease-in-out" | "bounce" | Function
 }
 
@@ -25,16 +26,19 @@ export default function Carousel(props: ICarouselProps) {
     pauseDuration = 2000,
     slideDuration = 1000,
     direction = 1,
+    pauseOnHover = true,
     tweenAnime = "ease",
   } = props
-  const [current, setCurrent] = useState(direction === 1 ? 1 : data.length);
+  const [current, setCurrent] = useState(direction === 1 ? 1 : data.length)
+  const pauseRef = useRef(false)
+  const leftRef = useRef(0)
   const sliderRef = useRef<HTMLDivElement>(null)
   const dataRef = useRef([
     data[data.length - 1],
     ...data,
     data[0]
   ])
-  const timer = useRef(0)
+  const timer = useRef(-1)
   const tweenFn = useMemo(() => {
     if (typeof tweenAnime === "function") {
       return tweenAnime
@@ -45,58 +49,83 @@ export default function Carousel(props: ICarouselProps) {
     }
   }, [tweenAnime])
 
-  useEffect(() => {
+  const init = () => {
     if (direction === 1) {
-      sliderRef.current!.style.left = `${-width}px`
+      leftRef.current = width
     } else {
-      sliderRef.current!.style.left = `${- (dataRef.current.length - 2) * width}px`
+      leftRef.current = (dataRef.current.length - 2) * width
     }
-  }, [])
+    sliderRef.current!.style.left = `${-leftRef.current}px`
+  }
+
+  useEffect(init, [])
 
   useEffect(() => {
-    timer.current = setTimeout(next, pauseDuration)
+    go(current)
     return () => clearTimeout(timer.current)
   }, [current])
 
-  const next = () => {
-    go(current + direction)
-  }
-
   const go = (index: number) => {
     const startTime = Date.now()
-    let left = 0
     //begin value
-    const b = current * width
+    const b = leftRef.current
     //change value
-    const c = (index - current) * width
+    const c = index * width - leftRef.current
+    if (c === 0) {
+      calcNext()
+      return;
+    }
     const anim = () => {
       const currTime = Math.min(slideDuration, Date.now() - startTime)
-      left = tweenFn(currTime, b, c, slideDuration)
-      if (currTime < slideDuration) {
-        sliderRef.current!.style.left = `${-left}px`
-        requestAnimationFrame(anim)
-      } else {
+      leftRef.current = tweenFn(currTime, b, c, slideDuration)
+      if (currTime === slideDuration) {
         // animation end, start next timer
-        calcNext(left)
+        calcNext()
+      } else {
+        sliderRef.current!.style.left = `${-leftRef.current}px`
+        requestAnimationFrame(anim)
       }
     }
     anim()
   }
 
-  const calcNext = (left: number) => {
-    if (left === (dataRef.current.length - 1) * width && direction === 1) {
-      sliderRef.current!.style.left = `${-width}px`
+  const calcNext = () => {
+    if (pauseOnHover && pauseRef.current) return;
+    if (leftRef.current >= (dataRef.current.length - 1) * width && direction === 1) {
+      leftRef.current = width
+      sliderRef.current!.style.left = `${-leftRef.current}px`
       setCurrent(1)
-    } else if (left === 0 && direction === -1) {
-      sliderRef.current!.style.left = `${- (dataRef.current!.length - 2) * width}px`
+    } else if (leftRef.current <= 0 && direction === -1) {
+      leftRef.current = (dataRef.current!.length - 2) * width
+      sliderRef.current!.style.left = `${-leftRef.current}px`
       setCurrent(dataRef.current!.length - 2)
     } else {
-      setCurrent(current + direction)
+      updateCurrentDelay(leftRef.current / width + direction)
     }
   }
 
+  const updateCurrentDelay = (v: number) => {
+    timer.current = setTimeout(() => setCurrent(v), pauseDuration)
+  }
+
+  const pauseTimer = () => {
+    if (!pauseOnHover) return;
+    pauseRef.current = true
+    clearTimeout(timer.current)
+    timer.current = -1
+  }
+
+  const resumeTimer = () => {
+    if (!pauseOnHover) return;
+    pauseRef.current = false
+    calcNext()
+  }
+
   return (
-    <div className='carousel' style={{width: `${width}px`, height: `${height}px`}}>
+    <div className='carousel' style={{width: `${width}px`, height: `${height}px`}}
+      onMouseEnter={pauseTimer}
+      onMouseLeave={resumeTimer}
+    >
       <div className='slider' ref={sliderRef} style={{width: `${dataRef.current.length * width}px`}}>
         {
           dataRef.current.map((img, index) => (
